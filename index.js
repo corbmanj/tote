50d987665d03068d62d2de804b69a83b459aaf5b
Jordan Corbman <corbmanj@gmail.com>
2017-12-18 14:24:48 -0600

    removed react router (#16)

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 
 const element = (
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-  </Router>
+  <App/>
 )
 
 render(element, document.getElementById('app'))
